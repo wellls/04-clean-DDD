7922cfcdfc52c7f8c6d1ffaa91790d172ce985b2
Wellington <wellington.map@hotmail.com>
2025-03-25 14:50:08 -0300

    mapping props

--- src/domain/entities/Answer.ts
+++ src/domain/entities/Answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from "../../core/entities/Entity";
+import { UniqueEntityID } from "../../core/entities/value-objects/UniqueEntityID";
 
 interface AnswerProps {
   content: string;
-  authorId: string;
-  questionId: string;
+  authorId: UniqueEntityID;
+  questionId: UniqueEntityID;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Answer extends Entity<AnswerProps> {
--- src/domain/entities/Question.ts
+++ src/domain/entities/Question.ts
@@ -1,11 +1,15 @@
 import { Entity } from "../../core/entities/Entity";
+import { UniqueEntityID } from "../../core/entities/value-objects/UniqueEntityID";
 import { Slug } from "./value-objects/Slug";
 
 interface QuestionProps {
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
   title: string;
   content: string;
   slug: Slug;
-  authorId: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {}
