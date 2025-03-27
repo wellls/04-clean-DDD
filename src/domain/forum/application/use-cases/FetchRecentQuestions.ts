8d084a085f26753e0ffda1f6e8c85e556072ea5b
Wellington <wellington.map@hotmail.com>
2025-03-27 20:41:05 -0300

    fetch recent question use case

---
+++ src/domain/forum/application/use-cases/FetchRecentQuestions.ts
@@ -0,0 +1,24 @@
+import { Question } from '../../enterprise/entities/Question'
+import { QuestionsRepository } from '../repositories/QuestionsRepository'
+
+interface FetchRecentQuestionsUseCaseRequest {
+  page: number
+}
+
+interface FetchRecentQuestionsUseCaseResponse {
+  questions: Question[]
+}
+
+export class FetchRecentQuestionsUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    page,
+  }: FetchRecentQuestionsUseCaseRequest): Promise<FetchRecentQuestionsUseCaseResponse> {
+    const questions = await this.questionsRepository.findManyRecent({ page })
+
+    return {
+      questions,
+    }
+  }
+}
